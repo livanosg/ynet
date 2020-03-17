d66d68204ee734a279c60c4b567cbef18297a011
livanos <livanos.g@gmail.com>
2020-03-17 18:16:51 +0200

    fix loading

--- loss_fn.py
+++ loss_fn.py
@@ -11,19 +11,19 @@ def weighted_crossentropy(predictions, labels):
         wce = tf.negative(tf.div(wce, tf.reduce_sum(class_frequencies)))
         return wce
 
-
 def weighted_log_dice_loss(predictions, labels):
     """Generalised Dice overlap as a deep learning loss function for highly unbalanced segmentations.
     Carole H. Sudre, Wenqi Li, Tom Vercauteren, Sebastien Ourselin, and M. Jorge Cardoso"""
 
     with tf.name_scope('Generalized_Dice_Loss'):
         class_frequencies = tf.reduce_sum(labels, axis=[0, 1, 2])
-        weights = tf.math.divide(1., tf.add(tf.pow(class_frequencies, 2), eps))  # epsilon()
-        numerator = tf.reduce_sum(tf.multiply(tf.reduce_sum(tf.multiply(predictions, labels), axis=(0, 1, 2)), weights)) + eps
-        denominator = tf.reduce_sum(tf.multiply(tf.reduce_sum(labels + predictions, axis=(0, 1, 2)), weights)) + eps
-        loss = tf.negative(tf.math.log(tf.multiply(tf.div(numerator, denominator), 2.0)))
-    return loss
-
+        weights = tf.math.divide(1., tf.pow(tf.add(class_frequencies, 1), 2))
+        numerator = tf.math.multiply(2., tf.math.add(tf.reduce_sum(tf.math.multiply(labels, predictions), axis=[0, 1, 2]), 1e-7))
+        # numerator = tf.reduce_sum(tf.multiply(tf.reduce_sum(tf.multiply(labels, predictions) + 1, axis=(0, 1, 2)), weights)) + epsilon()
+        denominator = tf.math.add(tf.reduce_sum(tf.math.add(labels, predictions), axis=[0, 1, 2]), 1e-7)
+        dice = tf.math.multiply(weights, tf.math.divide(numerator, denominator))
+        # denominator = tf.reduce_sum(tf.multiply(tf.reduce_sum(labels + predictions, axis=(0, 1, 2)), weights))
+    return tf.reduce_mean(tf.math.pow(tf.negative(tf.math.log(dice)), 0.3))
 
 def custom_loss(predictions, labels):
     with tf.name_scope('Custom_loss'):
